9aa3ad288a030831f44abcde9e8d899728000823
hafizulf <hafizulf68@gmail.com>
2021-10-07 15:17:56 +0700

    add village model and table villages migration

---
+++ models/village.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const Village = sequelize.define('Village', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    tableName: 'villages'
+  });
+
+  return Village;
+};
