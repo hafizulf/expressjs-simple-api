c82f2184dadafde3b5c3890a9bb2dc0a9d9cb493
hafizulf <hafizulf68@gmail.com>
2021-10-07 17:16:29 +0700

    add '/api/v1' as url prefix

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(villagesRouter);
+app.use('/api/v1', villagesRouter);
 
 module.exports = app;
