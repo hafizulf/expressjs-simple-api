b51a25ee8c42906e113307d99bcf2432fbf1804a
hafizulf <hafizulf68@gmail.com>
2021-10-07 21:33:09 +0700

    set cors and init auth routes

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 var villagesRouter = require('./routes/villages');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', authRouter);
 app.use('/api/v1', villagesRouter);
 
 module.exports = app;
